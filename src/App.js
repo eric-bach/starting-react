f85c075d6b97d3b8e6cacbee294fbb7a04be7133
Eric Bach <bach.eric@gmail.com>
2021-07-11 14:45:03 -0600

    Add emotion episode 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
 import './App.css';
 
 const PokemonRow = ({ pokemon, onSelect }) => (
@@ -50,6 +52,25 @@ PokemonInfo.propTypes = {
   }),
 };
 
+const Title = styled.h1`
+  text-align: center;
+`;
+const TwoColumnLayout = styled.div`
+  display: grid,
+  grid-template-columns: 70% 30%,
+  grid-column-gap: 1rem,
+`;
+const Container = styled.div`
+  margin: auto,
+  width: 800px,
+  padding-top: 1rem,
+`;
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2em;
+`;
+
 function App() {
   const [filter, filterSet] = React.useState('');
   const [pokemon, pokemonSet] = React.useState([]);
@@ -62,24 +83,12 @@ function App() {
   }, []);
 
   return (
-    <div
-      style={{
-        margin: 'auto',
-        width: 800,
-        paddingTop: '1rem',
-      }}
-    >
-      <h1 className='title'>Pokemon Search</h1>
+    <Container>
+      <Title>Pokemon Search</Title>
 
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '70% 30%',
-          gridColumnGap: '1rem',
-        }}
-      >
+      <TwoColumnLayout>
         <div>
-          <input
+          <Input
             value={filter}
             onChange={(event) => filterSet(event.target.value)}
           />
@@ -109,8 +118,8 @@ function App() {
           </table>
         </div>
         {selectedItem && <PokemonInfo {...selectedItem} />}
-      </div>
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
