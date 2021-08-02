8724ec950e4729f6a3eadd8e05a4a07d88b4bf76
Eric Bach <bach.eric@gmail.com>
2021-08-02 10:33:32 -0600

    Add Zustand

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { CssBaseline } from '@material-ui/core';
-import { createStore } from 'redux';
-import { Provider, useSelector, useDispatch } from 'react-redux';
 
 import './App.css';
 
 import PokemonInfo from './components/PokemonInfo';
 import PokemonFilter from './components/PokemonFilter';
 import PokemonTable from './components/PokemonTable';
-
-const pokemonReducer = (
-  state = { pokemon: [], filter: '', selectedPokemon: null },
-  action
-) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return { ...state, filter: action.payload };
-    case 'SET_POKEMON':
-      return { ...state, pokemon: action.payload };
-    case 'SET_SELECTED_POKEMON':
-      return { ...state, selectedPokemon: action.payload };
-    default:
-      // Return state instead of throwing error in Redux
-      return state;
-  }
-};
-
-const store = createStore(pokemonReducer);
 
 const Title = styled.h1`
   text-align: center;
@@ -44,19 +23,6 @@ const TwoColumnLayout = styled.div`
 `;
 
 function App() {
-  const dispatch = useDispatch();
-  const pokemon = useSelector((state) => state.pokemon);
-
-  React.useEffect(() => {
-    fetch('/starting-react/pokemon.json')
-      .then((resp) => resp.json())
-      .then((data) => dispatch({ type: 'SET_POKEMON', payload: data }));
-  }, []);
-
-  if (!pokemon) {
-    return <div>Loading data</div>;
-  }
-
   return (
     <PageContainer>
       <CssBaseline />
@@ -72,8 +38,4 @@ function App() {
   );
 }
 
-export default () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+export default App;
--- src/components/PokemonTable.jsx
+++ src/components/PokemonTable.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+
+import useStore from '../store';
 
 import PokemonRow from '../components/PokemonRow';
 
 const PokemonTable = () => {
-  const dispatch = useDispatch();
-  const pokemon = useSelector((state) => state.pokemon);
-  const filter = useSelector((state) => state.filter);
+  const pokemon = useStore((state) => state.pokemon);
+  const filter = useStore((state) => state.filter);
+  const setSelectedPokemon = useStore((state) => state.setSelectedPokemon);
 
   return (
     <table width='100%'>
@@ -19,9 +20,7 @@ const PokemonTable = () => {
           .map((pokemon) => (
             <PokemonRow
               pokemon={pokemon}
-              onClick={(pokemon) =>
-                dispatch({ type: 'SET_SELECTED_POKEMON', payload: pokemon })
-              }
+              onClick={(pokemon) => setSelectedPokemon(pokemon)}
             />
           ))}
       </tbody>
