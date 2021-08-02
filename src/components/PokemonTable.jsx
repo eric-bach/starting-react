1bfd48176b69a0c4ed9548d80f11ef724d705f14
Eric Bach <bach.eric@gmail.com>
2021-08-02 10:50:06 -0600

    MobX

--- src/components/PokemonTable.jsx
+++ src/components/PokemonTable.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 
-import useStore from '../store';
+import store from '../store';
 
 import PokemonRow from '../components/PokemonRow';
 
 const PokemonTable = () => {
-  const pokemon = useStore((state) => state.pokemon);
-  const filter = useStore((state) => state.filter);
-  const setSelectedPokemon = useStore((state) => state.setSelectedPokemon);
-
   return (
     <table width='100%'>
       <tbody>
-        {pokemon
-          .filter(({ name: { english } }) =>
-            english.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
-          )
-          .slice(0, 20)
-          .map((pokemon) => (
-            <PokemonRow
-              pokemon={pokemon}
-              onClick={(pokemon) => setSelectedPokemon(pokemon)}
-            />
-          ))}
+        {store.filteredPokemon.slice(0, 20).map((pokemon) => (
+          <PokemonRow
+            key={pokemon.id}
+            pokemon={pokemon}
+            onClick={(pokemon) => store.setSelectedPokemon(pokemon)}
+          />
+        ))}
       </tbody>
     </table>
   );
 };
 
-export default PokemonTable;
+export default observer(PokemonTable);
